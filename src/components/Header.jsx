2afee6433ec5727cc6dbbdb4cac8c06801b486b6
SUBINSON <fraisecheris@gmail.com>
2022-12-13 16:51:48 +0900

    👀 Style: modify sth

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,6 @@ export default function Header() {
   return (
     <div>
       <img className="ogu" src="ogu.png" alt="ogu" />
-      <img className="bar" src="bars-solid.svg" alt="nav"></img>
     </div>
   );
 }
