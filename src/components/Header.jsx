a33d908a33180d9e76ead2406b638b4f2c624a88
SUBINSON <fraisecheris@gmail.com>
2022-12-13 16:46:23 +0900

    👀 Style: add alt prop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header() {
   return (
     <div>
-      <img className='ogu' src="ogu.png" />
+      <img className="ogu" src="ogu.png" alt="ogu" />
       <img className="bar" src="bars-solid.svg" alt="nav"></img>
     </div>
   );
