3523aeb454a4d0bd54563c9fbcc54ae6256c2401
SUBINSON <fraisecheris@gmail.com>
2022-12-13 17:28:17 +0900

    👀 Style: resizing components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header() {
   return (
     <div>
-      <img className="ogu" src="ogu.png" alt="ogu" />
+      <img className="ogu" src="https://item.kakaocdn.net/do/3ea70cbb133ef01c1a4cb9d5eeebdd7a9f17e489affba0627eb1eb39695f93dd" alt="ogu" />
     </div>
   );
 }
